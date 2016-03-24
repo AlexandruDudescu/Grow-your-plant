97b8bf71ce8959e41a317d6b3934ee38b0affb27
AlexandruDudescu <alexdudescu@hotmail.com>
2016-03-24 18:46:25 +0200

    Added basic real-time functionality

---
+++ Grow your plant/App_Start/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Grow_your_plant.App_Start.Startup))]
+
+namespace Grow_your_plant.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ Grow your plant/Hubs/PlantStatusHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace Grow_your_plant.Hubs
+{
+    public class PlantStatusHub : Hub
+    {
+        public void Hello()
+        {
+        }
+    }
+}
